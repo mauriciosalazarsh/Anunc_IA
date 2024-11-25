4cbdcc0f2d9c69b11db31139274b71cc5f09b637
christophernajarro <christopher.najarro@utec.edu.pe>
2024-11-25 00:48:20 -0500

    DEPLOY

--- backend/common/utils/session_manager.py
+++ backend/common/utils/session_manager.py
@@ -4,16 +4,23 @@ import os
 
 SESSION_TIMEOUT = timedelta(minutes=30)
 
+# Conexión global
+redis_client = None
+
 class SessionManager:
     def __init__(self, redis_url=None):
+        global redis_client
         if redis_url is None:
             redis_url = os.getenv("REDIS_URL", "redis://redis:6379")  # Usa 'redis' si está en Docker
         print(f"Conectando a Redis en: {redis_url}")
-        try:
-            self.redis = aioredis.from_url(redis_url, decode_responses=True)
-        except Exception as e:
-            print(f"Error conectando a Redis: {e}")
-            raise
+        
+        if redis_client is None:
+            try:
+                redis_client = aioredis.from_url(redis_url, decode_responses=True)
+            except Exception as e:
+                print(f"Error conectando a Redis: {e}")
+                raise
+        self.redis = redis_client
 
     async def store_jwt(self, session_id: str, jwt_token: str):
         try:
