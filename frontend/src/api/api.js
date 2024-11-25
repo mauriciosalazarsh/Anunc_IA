e3923d43b738ddff529aa1d858fcd56904485747
christophernajarro <christopher.najarro@utec.edu.pe>
2024-11-25 11:19:52 -0500

    Deploy frontend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://anuncia.tech/", // Cambia al dominio correcto
+    baseURL: "https://kvpkimi65a3umrcqhbfnkqg2ma0yeare.lambda-url.us-east-2.on.aws", // Cambia al dominio correcto
     headers: {
         "Content-Type": "application/json",
     },
